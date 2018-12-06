58153394cdde71cc881a977df94460346eb5b3a8
MikeKowalsky <mkowalewski83@gmail.com>
2018-12-06 16:59:46 +0100

    Finished React

---
+++ public/build/precache-manifest.66fa947b5add0da0ea5632ba942732a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37a39b35e699355acde6016e5ce69039",
+    "url": "/static/media/logo.37a39b35.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6286ae020c1780aca81f",
+    "url": "/static/js/main.6286ae02.chunk.js"
+  },
+  {
+    "revision": "79d3865a8e381c8e602d",
+    "url": "/static/js/1.79d3865a.chunk.js"
+  },
+  {
+    "revision": "6286ae020c1780aca81f",
+    "url": "/static/css/main.b1f29b31.chunk.css"
+  },
+  {
+    "revision": "d1a0e9811fa5cd518c3755cc4ec9869b",
+    "url": "/index.html"
+  }
+];
